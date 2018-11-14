20ad51ef87f996e541fc24a8242a1b8ab14298c0
lingyi lu <lylu05uw@gmail.com>
2018-11-14 18:45:42 -0500

    change hostname type

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+const hostname = 'heroku.com';
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
-  hostname = '127.0.0.1';
+  //hostname = '127.0.0.1';
 }
 
 const server = http.createServer((req, res) => {
