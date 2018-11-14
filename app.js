eb4132ec09632c32f0d7a37bc26c941aba48ea0c
Fred <fredchenca@live.com>
2018-11-14 17:39:07 -0500

    update hostname

--- app.js
+++ app.js
@@ -1,30 +1,18 @@
-// const http = require('http');
+const http = require('http');
 
-// const hostname = '127.0.0.1';
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
+let hostname = 'heroku.com';
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+  hostname = '127.0.0.1';
+}
 
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World\n');
-// });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
 
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
