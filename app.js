28a726d299c63cf1f5388ae5cf679e992af1089b
lingyi lu <lylu05uw@gmail.com>
2018-11-14 19:00:06 -0500

    add global var

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const http = require('http');
-
-const hostname = 'heroku.com';
-let port = process.env.PORT;
+const hostname = 'herokuapp.com';
+const port = process.env.PORT;
 if (port == null || port == "") {
   port = 8000;
   //hostname = '127.0.0.1';
 }
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
 
-server.listen(port,hostname, function () {
+server.listen(port,hostname, ()=> {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
