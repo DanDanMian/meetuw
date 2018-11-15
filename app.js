fa0f85e61f7f7ec01f67065047663874fd41c923
lingyi lu <lylu05uw@gmail.com>
2018-11-14 19:06:22 -0500

    add console.log

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 const http = require('http');
 const hostname = 'herokuapp.com';
 const port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-  //hostname = '127.0.0.1';
-}
+console.log('port : ${port}');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
+
 
 server.listen(port,hostname, ()=> {
   console.log(`Server running at http://${hostname}:${port}/`);
