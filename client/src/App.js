79d8e7e55e9e14aab304fb85cae7b3712068d43e
MingYuanTina <bjteana@gmail.com>
2018-11-21 17:49:36 -0500

    New Landing layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {program: '', category: '' };
+
+    this.handleProgramChange = this.handleProgramChange.bind(this);
+    this.handleCateogryChange = this.handleCateogryChange.bind(this);
+  }
+
+  handleProgramChange(event){
+    this.setState({program: event.target.value});
+  }
+
+  handleCateogryChange(event){
+    this.setStatet({category: event.target.value});
+  }
+
+
   render() {
     return (
-      <div className="App">
-          TEST
-      </div>
+       <form onSubmit={this.handleSubmit}>
+       <label>I am a UW student in</label>
+        <label>
+          <input type="text" value={this.state.program} onChange={this.handleProgramChange} />
+           Program.
+        </label>
+        <label>I am looking for </label>
+        <label>
+          <input type="text" value={this.state.category} onChange={this.handleCateogryChange} /> 
+        </label>
+        <input type="submit" value="Submit" />
+        <input type="submit" value="Tryout" />
+      </form>
     );
   }
 }
