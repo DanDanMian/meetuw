c7734d19e41afd1706c80192b142bce62e5b1d19
MingYuanTina <bjteana@gmail.com>
2018-11-20 19:37:48 -0500

    react setup

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const http = require('http');
 
 const hostname = '127.0.0.1';// 'herokuapp.com';
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
 console.log(`current port: ${process.env.PORT}`);
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
 });
-
 
 server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
