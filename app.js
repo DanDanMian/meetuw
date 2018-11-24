74e1abe2ce0bc6543f128ac407aba90ff32b78dc
Da Wei <da.wei@hotmail.ca>
2018-11-23 19:17:57 -0500

    Convert app.js to ES

--- app.js
+++ app.js
@@ -1,61 +1,57 @@
-var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var bodyParser = require('body-parser');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
 const path = require('path');
 
 
-MongoClient.connect("mongodb://admin:0000@meetuw-shard-00-00-5sqfz.mongodb.net:27017,meetuw-shard-00-01-5sqfz.mongodb.net:27017,meetuw-shard-00-02-5sqfz.mongodb.net:27017/test?ssl=true&replicaSet=meetuw-shard-0&authSource=admin", function(err, db) {
-  if(!err) {
-    console.log("We are connected to DB");
+MongoClient.connect('mongodb://admin:0000@meetuw-shard-00-00-5sqfz.mongodb.net:27017,meetuw-shard-00-01-5sqfz.mongodb.net:27017,meetuw-shard-00-02-5sqfz.mongodb.net:27017/test?ssl=true&replicaSet=meetuw-shard-0&authSource=admin', (err, db) => {
+  if (!err) {
+    console.log('We are connected to DB');
   }
 });
 
-var app = express();
-var port = process.env.PORT;
+const app = express();
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const staticFiles = express.static(path.join(__dirname, './client/build'));
 app.use(staticFiles);
 
 
-app.get('/test', function(req, res){
+app.get('/test', (req, res) => {
   res.send('TEST');
 });
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.send('CONNECTED');
 });
 
-app.post('/testpost', function(req, res){
+app.post('/testpost', (req, res) => {
   console.log(req.body);
-	res.send({program: `${req.body.program}`, category: `${req.body.category}`});
+  res.send({ program: `${req.body.program}`, category: `${req.body.category}` });
 });
 
-app.post('/api/login', function(req, res){
+app.post('/api/login', (req, res) => {
   console.log(req.body);
-  if(req.body.email == 'test@uwaterloo.ca' && req.body.password == '12345'){
+  if (req.body.email == 'test@uwaterloo.ca' && req.body.password == '12345') {
     res.send('login successful');
-  }
-  else{
+  } else {
     res.send('Wrong Email or Password!');
   }
 });
 
-app.post('/match_request', function(req, res){
+app.post('/match_request', (req, res) => {
   console.log(req.body);
   res.send();
-})
+});
 
 app.get('*', staticFiles);
 
 
-app.listen(port, function(){
-  console.log('Run on port '+port);
+app.listen(port, () => {
+  console.log(`Run on port ${port}`);
   console.log(`Server running at http://127.0.0.1:${port}/`);
 });
-
-
-
