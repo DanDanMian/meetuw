fc7eee2ace21ee48433766473c32e6fedfda9eba
Derf <DanDanMian@users.noreply.github.com>
2019-03-11 02:28:53 -0400

    try to send email from heroku env

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,28 @@ const passport = require("../passport");
 router.post("/api/login", passport.authenticate("local"), (req, res) => {
   req.session.email = req.body.email;
 
+  var helper = require('sendgrid').mail;
+  var from_email = new helper.Email('app113928750@heroku.com');
+  var to_email = new helper.Email(req.body.email);
+  var subject = 'Confirm your MeetUW account';
+  var content = new helper.Content('text/plain', 'Click to confirm');
+  var mail = new helper.Mail(from_email, subject, to_email, content);
+
+  var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+  var request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON(),
+  });
+
+  sg.API(request, function(error, response){
+    console.log(response.statusCode);
+    console.log(response.body);
+    console.log(response.headers);
+  });
+
   res.send("SUCCESS");
+  console.log('after res.send');
 });
 
 router.post("/api/register", function(req, res) {
@@ -27,10 +48,34 @@ router.post("/api/register", function(req, res) {
       User.create(userObj, function(err, res) {
         if (err) throw err;
       });
+
+      //send email
+      var helper = require('sendgrid').mail;
+      var from_email = new helper.Email('app113928750@heroku.com');
+      var to_email = new helper.Email(email);
+      var subject = 'Confirm your MeetUW account';
+      var content = new helper.Content('text/plain', 'Click to confirm');
+      var mail = new helper.Mail(from_email, subject, to_email, content);
+
+      var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+      var request = sg.emptyRequest({
+        method: 'POST',
+        path: '/v3/mail/send',
+        body: mail.toJSON(),
+      });
+
+      sg.API(request, function(error, response){
+        console.log(response.statusCode);
+        console.log(response.body);
+        console.log(response.headers);
+      });
+
       res.send("SUCCESS");
+      console.log('res send success');
     }
   });
 });
+  
 
 // function ValidateEmail(mail)
 // {
@@ -43,3 +88,4 @@ router.post("/api/register", function(req, res) {
 // }
 
 module.exports = router;
+
