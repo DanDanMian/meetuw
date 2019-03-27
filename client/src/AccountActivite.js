342f2caa76e41ef109c8e88d76163d34abc84337
Derf <DanDanMian@users.noreply.github.com>
2019-03-27 02:25:24 -0400

    update activite page

--- client/src/AccountActivite.js
+++ client/src/AccountActivite.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 
 import Logo1 from "./picture/Logo1.png";
 import "./App.css";
+import Button from '@material-ui/core/Button';
 
 class AccountActive extends Component {
   constructor(props) {
@@ -23,23 +24,79 @@ class AccountActive extends Component {
       },
       body: JSON.stringify({ token: token })
     })
-      .then(response => response.text())
-      .then(data => {
-        console.log(data);
-        this.setState({ responseToPost: data });
-      });
+    .then(response => response.text())
+    .then(data => {
+      console.log(data);
+      this.setState({ responseToPost: data });
+    });
+
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
   }
 
+  handleLogin = async e => {
+    e.preventDefault();
+    this.props.history.push({
+      pathname: "/login"
+    });
+  };
+
+  handleRegister = async e => {
+    e.preventDefault();
+    this.props.history.push({
+      pathname: "/email"
+    });
+  };
+
   render() {
+    const linkOk = this.state.responseToPost;
+    let display;
+    if(linkOk === "SUCCESS"){
+      display = 
+      <div>
+        <h3 className="Text">
+          Your account is activited!
+        </h3>
+        <br />
+        <br />
+        <br />
+        <form onSubmit={this.handleLogin}>
+        <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+          Register Page
+          </Button>
+        </form>
+      </div>
+    }else{
+      display =
+      <div>
+        <h3 className="Text">
+          This link is invalid
+        </h3>
+        <br />
+        <br />
+        <br />
+        <form onSubmit={this.handleRegister}>
+        <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+          Register Page
+          </Button>
+        </form>
+      </div>
+    }
     return (
       <div className="App">
         <div>
           <div>
             <img src={Logo1} width="100" height="100" alt="Logo" />
           </div>
-          <h2 className="Logo">MeetUW</h2>
-          <p>{this.state.responseToPost}</p>
-          <a href="/">go to home</a>
+          {display}
         </div>
       </div>
     );
