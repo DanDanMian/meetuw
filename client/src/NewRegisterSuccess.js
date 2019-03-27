a0bb7e93a457d21bca7326d76c7048870b6a771b
Derf <DanDanMian@users.noreply.github.com>
2019-03-27 01:35:40 -0400

    update success register page

--- client/src/NewRegisterSuccess.js
+++ client/src/NewRegisterSuccess.js
@@ -3,6 +3,7 @@ import { withRouter } from "react-router-dom";
 
 import Logo1 from "./picture/Logo1.png";
 import "./App.css";
+import Button from '@material-ui/core/Button';
 
 class NewRegisterSuccess extends Component {
   constructor(props) {
@@ -12,19 +13,27 @@ class NewRegisterSuccess extends Component {
       submitted: false
     };
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
   }
 
-  handleSubmit = async e => {
+  handleLogin = async e => {
     e.preventDefault();
     this.setState({ submitted: true }, () => {
       if (this.state.submitted) {
         this.props.history.push({
-          pathname: "/academic",
-          state: {
-            name: this.props.location.state.name,
-            email: this.props.location.state.email
-          }
+          pathname: "/login"
+        });
+      }
+    });
+  };
+
+  handleRegister = async e => {
+    e.preventDefault();
+    this.setState({ submitted: true }, () => {
+      if (this.state.submitted) {
+        this.props.history.push({
+          pathname: "/email"
         });
       }
     });
@@ -39,18 +48,38 @@ class NewRegisterSuccess extends Component {
           </div>
           <h2 className="Logo">MeetUW</h2>
         </div>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleLogin}>
           <h3 className="Text">
-            Successfully Registered. You can start to explore now!
+            Successfully Registered. <br /> 
+            Wait for your activation email: ...@uwaterloo.ca. <br />
+            It may take several minutes. <br />
+            Don't forget to check your junk folder.
           </h3>
           <br />
           <br />
           <br />
-          <input
+          <Button
             type="submit"
-            value="Let's Rock"
-            onChange={this.handleSubmit}
-          />
+            variant="contained"
+            color="primary"
+          >
+            Go To Sign In Page
+          </Button>
+          </form>
+          <form onSubmit={this.handleRegister}>
+          <h3 className="Text">
+            Wrong email?
+          </h3>
+          <br />
+          <br />
+          <br />
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Go To Register Page
+          </Button>
         </form>
       </div>
     );
